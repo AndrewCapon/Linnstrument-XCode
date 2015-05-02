5100fa527eaca1a215feee08bcc879f6bcd62b09
Andrew Capon <andrewcapon@MP12.local>
2015-05-02 09:45:41 +0100

    Added makefile for Xcode

---
+++ linnstrument-doc/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  linnstrument-doc
+//
+//  Created by Andrew Capon on 10/04/2015.
+//  Copyright (c) 2015 Ctrl Alt Delete Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+  
+}
